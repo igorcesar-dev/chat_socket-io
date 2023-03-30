ffe041837e130ce3c46dffb3db62f830cae6eb06
igorcesar-dev <igorcesar.dev@gmail.com>
2023-03-30 20:31:39 -0300

    exhibition_message

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// EVENTO DE CONEXÃO, "socket" = cliente
+io.on("connection", (socket) => {
+    socket.on("disconnect", () => {
+        console.log("x desconectou: " + socket.id);
+    })
+
+  socket.on("msg", (data) => {
+    socket.emit("showmsg", data);
+    console.log(data);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("app rodando");
+});
